c116c96e30bd5f13276b36b61f961384158fc72c
Prakash Pandey <prakash.pandey@openlabs.co.in>
2014-07-22 12:23:44 +0530

    Migrated to 3.2 #4943

--- tests/test_views_depends.py
+++ tests/test_views_depends.py
@@ -30,6 +30,7 @@ class TestViewsDepends(unittest.TestCase):
         """
         trytond.tests.test_tryton.install_module('invoice_payment_gateway')
 
+    @unittest.skip("No views defined yet")
     def test0005views(self):
         '''
         Test views.
